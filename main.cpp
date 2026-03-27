829c29e695aea313cd760f945cec4353dedc9c94
albsure4 <tico.lorie@gmail.com>
2026-03-27 01:05:54 -0400

    Implemented quicksort

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@
 #include <vector>
 #include <sstream>
 
-#include "dataEntries.h"
-
+#include "DataEntries.h"
+#include "QuickSort.h"
 
 void handleInternalCommas(std::stringstream& stream, std::string& parsed, std::string garbage) {
     while(parsed[0] == '"' and parsed.back() != '"'){
@@ -26,7 +26,8 @@ while (getline(readFile, text)){
 */
 
 std::cout << "Welcome to our Project!\n\n";
-std::cout << "Please select a country by typing the corresponding number:\n";
+std::cout << "* In order to make a selection, please type the number corresponding to it *\n\n";
+std::cout << "Please select a country:\n";
 std::cout << "1. United States\n2. Brazil\n3. Canada\n4. Germany\n5. France\n6. United Kingdom\n7. India\n8. Japan\n9. South Korea\n10. Mexico\n11. Russia\n\n";
 
 std::string country;
@@ -80,7 +81,7 @@ if (!readFile.is_open()) {
     std::cerr << "Could not open file!" << std::endl; //error message if data set is not accessed
 }
 
-std::vector<dataEntries> trending; //create vector of class objects where an object is a singular trending video
+std::vector<dataEntries> listOfVideos; //create vector of class objects where an object is a singular trending video
 getline(readFile, text); //ignore the first line which has the column titles
 while (getline(readFile, text)){
     std::stringstream stream(text);
@@ -117,19 +118,51 @@ while (getline(readFile, text)){
             handleInternalCommas(stream, garbage, garbage);
         }
     }
-std::cout << title << std::endl;
+/*std::cout << title << std::endl;
     std::cout << views << std::endl;
     std::cout << likes << std::endl;
     std::cout << comments << std::endl;
     std::cout << category << std::endl;
     std::cout << channel << std::endl << std::endl;
-    std::cout << stream.str() << std::endl;
+    std::cout << stream.str() << std::endl;*/
     dataEntries trendingVideo(title, stoi(views), stoi(likes), stoi(comments), stoi(category), channel);
-    trending.push_back(trendingVideo);
+    listOfVideos.push_back(trendingVideo);
 }
-getline(readFile,text);
-std::cout << text << std::endl;
 
+std::cout << "Select a sorting algorithm\n\n";
+std::cout << "1. HeapSort\n";
+std::cout << "2. QuickSort\n";
+
+//Take in User input for sorting algorithm type
+std::string algo;
+std::cin >> algo;
+
+std::cout << "Select an option to sort by\n\n";
+std::cout << "1. View Count\n";
+std::cout << "2. Like Count\n";
+std::cout << "3. Comment Count\n";
+
+//Take in User input for what they want to sort by
+std::string sortByType;
+std::cin >> sortByType;
+
+std::cout << "Select how you would like to display the top 10\n\n";
+std::cout << "1. Channel Name\n";
+std::cout << "2. Category\n";
+std::cout << "3. Video Name\n";
+
+//Take in User input for what they want to sort by
+std::string displayType;
+std::cin >> displayType;
+if (stoi(algo) == 2) {
+    int n = listOfVideos.size();
+    quickSort(sortByType, listOfVideos, 0, n - 1);
+    if (stoi(sortByType) == 1) {
+        for (int i = 0; i < 10; i++) {
+            std::cout << listOfVideos[i].channel;
+        }
+    }
+}
 readFile.close();
 return 0;
 }
