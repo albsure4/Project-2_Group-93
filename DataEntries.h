f60736181de58c54fdf2af3e54ac3278549ea7b6
Tanner Fulwider <t.fulwider@ufl.edu>
2026-03-28 01:19:33 -0400

    Updated UI to completion

--- DataEntries.h
+++ DataEntries.h
@@ -15,12 +15,76 @@ int views;
 int likes;
 int comments;
 int category;
+std::string categoryName;
 std::string channel;
 
     dataEntries(std::string  Title,const int& Views,const int& Likes,const int& Comments,const int& Category, std::string  Channel) : title(std::move(Title)),
-views(Views), likes(Likes), comments(Comments), category(Category), channel(std::move(Channel)){};
+views(Views), likes(Likes), comments(Comments), category(Category), channel(std::move(Channel)) {
+        if (category == 1)
+            categoryName = "Film & Animation";
+        if (category == 2)
+            categoryName = "Autos & Vehicles";
+        if (category == 10)
+            categoryName = "Music";
+        if (category == 15)
+            categoryName = "Pets & Animals";
+        if (category == 17)
+            categoryName = "Sports";
+        if (category == 18)
+            categoryName = "Short Movies";
+        if (category == 19)
+            categoryName = "Travel & Events";
+        if (category == 20)
+            categoryName = "Gaming";
+        if (category == 21)
+            categoryName = "Videoblogging";
+        if (category == 22)
+            categoryName = "People & Blogs";
+        if (category == 23)
+            categoryName = "Comedy";
+        if (category == 24)
+            categoryName = "Entertainment";
+        if (category == 25)
+            categoryName = "News & Politics";
+        if (category == 26)
+            categoryName = "Howto & Style";
+        if (category == 27)
+            categoryName = "Education";
+        if (category == 28)
+            categoryName = "Science & Technology";
+        if (category == 29)
+            categoryName = "Nonprofits & Activism";
+        if (category == 30)
+            categoryName = "Movies";
+        if (category == 31)
+            categoryName = "Anime/Animation";
+        if (category == 32)
+            categoryName = "Action/Adventure";
+        if (category == 33)
+            categoryName = "Classics";
+        if (category == 34)
+            categoryName = "Comedy";
+        if (category == 35)
+            categoryName = "Documentary";
+        if (category == 36)
+            categoryName = "Drama";
+        if (category == 37)
+            categoryName = "Family";
+        if (category == 38)
+            categoryName = "Foreign";
+        if (category == 39)
+            categoryName = "Horror";
+        if (category == 40)
+            categoryName = "Sci-Fi/Fantasy";
+        if (category == 41)
+            categoryName = "Thriller";
+        if (category == 42)
+            categoryName = "Shorts";
+        if (category == 43)
+            categoryName = "Shows";
+        if (category == 44)
+            categoryName = "Trailers";
+    };
 };
-
-
 
 #endif //DATAENTRIES_H
--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <vector>
 #include <sstream>
+#include <chrono>
 
 #include "DataEntries.h"
 #include "QuickSort.h"
@@ -17,6 +18,63 @@ void handleInternalCommas(std::stringstream& stream, std::string& parsed, std::s
     }
 }
 
+void readFromFile(std::string fileName, std::vector<dataEntries>& videos) {
+    std::string text;
+    std::ifstream inFile(fileName);
+    if (!inFile.is_open()) {
+        std::cerr << "Could not open file!" << std::endl; //error message if data set is not accessed
+        return;
+    }
+
+    getline(inFile, text); //ignore the first line which has the column titles
+    while (getline(inFile, text)){
+        std::stringstream stream(text);
+        std::string title;
+        std::string views;
+        std::string likes;
+        std::string comments;
+        std::string category;
+        std::string channel;
+        std::string garbage; //store video data we don't need here to keep parsing
+        for (int i = 0; i < 13; i++) {
+            if (i == 2) {
+                getline(stream, title, ',');
+                handleInternalCommas(stream, title, garbage);
+            }
+            else if (i == 3) {
+                getline(stream, channel, ',');
+                handleInternalCommas(stream, channel, garbage);
+            }
+            else if (i == 4) {
+                getline(stream, views, ',');
+            }
+            else if (i == 5) {
+                getline(stream, likes, ',');
+            }
+            else if (i == 8) {
+                getline(stream, category, ',');
+            }
+            else if (i == 10) {
+                getline(stream, comments, ',');
+            }
+            else {
+                getline(stream, garbage, ',');
+                handleInternalCommas(stream, garbage, garbage);
+            }
+        }
+        /*std::cout << title << std::endl;
+            std::cout << views << std::endl;
+            std::cout << likes << std::endl;
+            std::cout << comments << std::endl;
+            std::cout << category << std::endl;
+            std::cout << channel << std::endl << std::endl;
+            std::cout << stream.str() << std::endl;*/
+        dataEntries trendingVideo(title, stoi(views), stoi(likes), stoi(comments), stoi(category), channel);
+        videos.push_back(trendingVideo);
+    }
+    inFile.close();
+}
+
 int main(){
 
 /*
@@ -25,65 +83,79 @@ while (getline(readFile, text)){
     std::cout << text;
 }
 */
+std::vector<dataEntries> listOfVideos; //create vector of class objects where an object is a singular trending video
 
 std::cout << "Welcome to our Project!\n\n";
 std::cout << "* In order to make a selection, please type the number corresponding to it *\n\n";
 std::cout << "Please select a country:\n";
-std::cout << "1. United States\n2. Brazil\n3. Canada\n4. Germany\n5. France\n6. United Kingdom\n7. India\n8. Japan\n9. South Korea\n10. Mexico\n11. Russia\n\n";
+std::cout << "1. United States\n2. Brazil\n3. Canada\n4. Germany\n5. France\n6. United Kingdom\n7. India\n8. Japan\n9. South Korea\n10. Mexico\n11. Russia\n12. All\n\n";
 
 std::string country;
-std::cin >> country;
-std::string text;
+while (true){ // make sure number is between 1-11, else: retake input
+    std::cin >> country;
+    if (stoi(country) > 12 || stoi(country) < 1){
+        std::cerr << "Incorrect Input- Must be a number 1-11\n";
+        continue;
+    }
+    break;
+}
 
 std::string fileName;
 
-//
 if (country == "1") {
-    fileName = "Data/US_Trending.csv";
+    readFromFile("Data/US_Trending.csv", listOfVideos);
 }
 else if (country == "2") {
-    fileName = "Data/BR_Trending.csv";
+    readFromFile("Data/BR_Trending.csv", listOfVideos);
 }
 else if (country == "3") {
-    fileName = "Data/CA_Trending.csv";
+    readFromFile("Data/CA_Trending.csv", listOfVideos);
 }
 else if (country == "4") {
     std::cout << "hello";
-    fileName = "Data/DE_Trending.csv";
+    readFromFile("Data/DE_Trending.csv", listOfVideos);
 }
 else if (country == "5") {
-    fileName = "Data/FR_Trending.csv";
+    readFromFile("Data/FR_Trending.csv", listOfVideos);
 }
 else if (country == "6") {
-    fileName = "Data/GB_Trending.csv";
+    readFromFile("Data/GB_Trending.csv", listOfVideos);
 }
 else if (country == "7") {
-    fileName = "Data/IN_Trending.csv";
+    readFromFile("Data/IN_Trending.csv", listOfVideos);
 }
 else if (country == "8") {
-    fileName = "Data/JP_Trending.csv";
+    readFromFile("Data/JP_Trending.csv", listOfVideos);
 }
 else if (country == "9") {
-    fileName = "Data/KR_Trending.csv";
+    readFromFile("Data/KR_Trending.csv", listOfVideos);
 }
 else if (country == "10") {
-    fileName = "Data/MX_Trending.csv";
+    readFromFile("Data/MX_Trending.csv", listOfVideos);
 }
 else if (country == "11") {
-    fileName = "Data/RU_Trending.csv";
+    readFromFile("Data/RU_Trending.csv", listOfVideos);
 }
-else {
-    std::cerr << "Incorrect Input- Must be a number 1-11\n\n";
+else if (country == "12"){
+    readFromFile("Data/US_Trending.csv", listOfVideos);
+    readFromFile("Data/BR_Trending.csv", listOfVideos);
+    readFromFile("Data/CA_Trending.csv", listOfVideos);
+    readFromFile("Data/DE_Trending.csv", listOfVideos);
+    readFromFile("Data/FR_Trending.csv", listOfVideos);
+    readFromFile("Data/GB_Trending.csv", listOfVideos);
+    //readFromFile("Data/IN_Trending.csv", listOfVideos);
+    readFromFile("Data/JP_Trending.csv", listOfVideos);
+    readFromFile("Data/KR_Trending.csv", listOfVideos);
+    readFromFile("Data/MX_Trending.csv", listOfVideos);
+    readFromFile("Data/RU_Trending.csv", listOfVideos);
 }
 
 std::ifstream readFile(fileName);
 
-if (!readFile.is_open()) {
-    std::cerr << "Could not open file!" << std::endl; //error message if data set is not accessed
-}
+if (listOfVideos.empty())
+    return 0;
 
-std::vector<dataEntries> listOfVideos; //create vector of class objects where an object is a singular trending video
-getline(readFile, text); //ignore the first line which has the column titles
+/*getline(readFile, text); //ignore the first line which has the column titles
 while (getline(readFile, text)){
     std::stringstream stream(text);
     std::string title;
@@ -119,16 +191,16 @@ while (getline(readFile, text)){
             handleInternalCommas(stream, garbage, garbage);
         }
     }
-/*std::cout << title << std::endl;
-    std::cout << views << std::endl;
-    std::cout << likes << std::endl;
-    std::cout << comments << std::endl;
-    std::cout << category << std::endl;
-    std::cout << channel << std::endl << std::endl;
-    std::cout << stream.str() << std::endl;*/
+    /*std::cout << title << std::endl;
+        std::cout << views << std::endl;
+        std::cout << likes << std::endl;
+        std::cout << comments << std::endl;
+        std::cout << category << std::endl;
+        std::cout << channel << std::endl << std::endl;
+        std::cout << stream.str() << std::endl;#1#
     dataEntries trendingVideo(title, stoi(views), stoi(likes), stoi(comments), stoi(category), channel);
     listOfVideos.push_back(trendingVideo);
-}
+}*/
 
 std::cout << "Select a sorting algorithm\n\n";
 std::cout << "1. HeapSort\n";
@@ -157,28 +229,45 @@ std::string displayType;
 std::cin >> displayType;
 if (stoi(algo) == 2) {
     int n = listOfVideos.size();
-    if (stoi(sortByType) == 1) {
+    auto start = std::chrono::high_resolution_clock::now(); // retrieved clock function from https://www.geeksforgeeks.org/cpp/measure-execution-time-function-cpp/
+    if (stoi(sortByType) == 1)
         quickSort("views", listOfVideos, 0, n - 1);
-        for (int i = 0; i < 10; i++) {
-            std::cout << i + 1 << ". " << listOfVideos[i].channel << std::endl;
-        }
-    }
+    if (stoi(sortByType) == 1)
+        quickSort("likes", listOfVideos, 0, n - 1);
+    if (stoi(sortByType) == 1)
+        quickSort("comments", listOfVideos, 0, n - 1);
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    std::cout << "QuickSort took " << duration.count() << " microseconds to complete.\n\n";
 }
 else if (stoi(algo) == 1) {
-    //int n = listOfVideos.size();
-    if (stoi(sortByType) == 1){
+    auto start = std::chrono::high_resolution_clock::now();
+    if (stoi(sortByType) == 1)
         heapSort("views", listOfVideos);
-        for (int i = 0; i < 10; i++) {
-            std::cout << i + 1 << ". " << listOfVideos[i].channel << std::endl;
-        }
-    }
-    else if (stoi(sortByType) == 2) {
+    else if (stoi(sortByType) == 2)
         heapSort("likes", listOfVideos);
-        for (int i = 0; i < 10; i++) {
-            std::cout << i + 1 << ". " << listOfVideos[i].channel << std::endl;
-        }
-    }
+    else if (stoi(sortByType) == 3)
+        heapSort("comments", listOfVideos);
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    std::cout << "HeapSort took " << duration.count() << " microseconds to complete.\n\n";
 }
-readFile.close();
+
+if (stoi(displayType) == 1)
+{
+    for (int i = 0; i < 10; i++)
+        std::cout << i + 1 << ". " << listOfVideos[i].channel << std::endl;
+}
+else if (stoi(displayType) == 2)
+{
+    for (int i = 0; i < 10; i++)
+        std::cout << i + 1 << ". " << listOfVideos[i].categoryName << std::endl;
+}
+else if (stoi(displayType) == 3)
+{
+    for (int i = 0; i < 10; i++)
+        std::cout << i + 1 << ". " << listOfVideos[i].title << std::endl;
+}
+
 return 0;
 }
